8c8ba8aed8efb437d71fa4cc2f185e9ace8562c7
mariuselix <marius.porcolean@gmail.com>
2017-04-16 16:51:30 +0300

    Added JS portfolio filtering

--- js/agency.js
+++ js/agency.js
@@ -30,4 +30,33 @@
         }
     })
 
+    // Portfolio filter
+    $(".cat-sel").click(function(){
+       var value = $(this).attr('data-filter');
+       if(value == "all") {
+           //$('.filter').removeClass('hidden');
+           $('.portfolio-item').show('1000');
+       }
+       else {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+           $(".portfolio-item").not('.'+value).hide('3000');
+           $('.portfolio-item').filter('.'+value).show('3000');
+
+       }
+    });
+
+    // Load More button
+    $(".portfolio-item").slice(0, 6).show();
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".portfolio-item:hidden").slice(0, 3).slideDown();
+        if ($(".portfolio-item:hidden").length == 0) {
+            $("#loadMore").fadeOut('slow');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top -200
+        }, 1000);
+    });
+
 })(jQuery); // End of use strict
